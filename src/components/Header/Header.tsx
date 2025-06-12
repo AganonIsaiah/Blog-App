43df16061286e8e7bc02e673e0a5a88dc6b895a3
AganonIsaiah <isaiahaganon0@gmail.com>
2025-06-12 17:38:34 -0400

    structuring

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,35 @@
+import styles from "./Header.module.css"
+
+export default function Header() {
+    return (
+        <header className={styles.header}>
+            <h1 className={styles.title}>Isaiah's Blog</h1>
+
+
+            <div className={styles.right}>
+
+                <div className={styles.link}>
+                    <img
+                        src="/versioncontrol.svg"
+                        alt="Version Control Icon"
+                        className={styles.icon}
+                    />
+                    <a
+                        href="https://github.com/AganonIsaiah/Blog-App"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className={styles.github}
+                    >AganonIsaiah/Blog-App</a>
+                </div>
+                <input
+                    type="text"
+                    placeholder="Search..."
+                    className={styles.search}
+                />
+
+
+            </div>
+        </header>
+
+    );
+}
